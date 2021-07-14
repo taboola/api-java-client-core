c252064053a25357d1f558129a32794e15d96848
nadavvitri <nadav325@gmail.com>
2021-07-14 10:25:56 +0300

    StringResponseFacrtories test

--- src/test/java/com/taboola/rest/api/internal/StringConverterFactoryTest.java
+++ src/test/java/com/taboola/rest/api/internal/StringConverterFactoryTest.java
@@ -13,11 +13,29 @@ import retrofit2.Converter;
  */
 public class StringConverterFactoryTest {
 
+    public static class EntityModelExample {
+        private String id;
+        private String name;
+    }
+
     @Test
     public void testResponseBodyConverter() {
         StringConverterFactory testInstance = StringConverterFactory.create(new StringResponseFactories());
 
         Converter<ResponseBody, ?> responseConverter = testInstance.responseBodyConverter(String.class, null, null);
+        Assert.assertNotNull("Invalid converter, expecting converter", responseConverter);
+
+        responseConverter = testInstance.responseBodyConverter(Object.class, null, null);
+        Assert.assertNull("Invalid converter, expecting no converter", responseConverter);
+    }
+
+    @Test
+    public void testResponseBodyConverterWithStringResponseFactory() {
+        StringResponseFactories stringResponseFactories = new StringResponseFactories();
+        stringResponseFactories.addFactory(EntityModelExample.class, null);
+        StringConverterFactory testInstance = StringConverterFactory.create(stringResponseFactories);
+
+        Converter<ResponseBody, ?> responseConverter = testInstance.responseBodyConverter(EntityModelExample.class, null, null);
         Assert.assertNotNull("Invalid converter, expecting converter", responseConverter);
 
         responseConverter = testInstance.responseBodyConverter(Object.class, null, null);
